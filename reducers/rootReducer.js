ad19a4c25a2c85715f927e599c69fefc2cd49f45
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-01-22 23:08:29 -0500

    Add files via upload

---
+++ reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState={
+    posts:[
+        {id:'1',title:'ffff',body:'ffdc'},
+        {id:'2',title:'aaff',body:'fdae'},
+        {id:'3',title:'ffkjff',body:'fdddxz'},
+
+    ]
+}
+const rootReducer = (state=initState,action)=>{
+    return state;
+}
+export default rootReducer
