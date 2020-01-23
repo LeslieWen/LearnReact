8b567b05046f622b4f5308a8280350f5bd360f49
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-01-22 22:12:35 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react';
-import Todos from './Todos'
-import AddTodo from './AddTodo'
+import React,{Component} from 'react';
+import Navbar from './components/Navbar'
+import {BrowserRouter,Route,Switch}from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component{
-  state={
-    todos: [
-      {id:1,content:'buy some hotdogs'},
-      {id:2,content:'buy some ketchup'}
-    ]
-  }
-  deleteTodo=(id)=>{
-    const todos=this.state.todos.filter(todo=>{
-      return todo.id !== id
-    });
-    this.setState({
-      todos:todos
-    })
-  }
-  addTodo=(todo)=>{
-    todo.id=Math.random();
-    let todos=[...this.state.todos,todo] //add new todo to list
-    this.setState({
-      todos:todos
-    })
-
-  }
-
   render(){
-    return (
-      <div className="todo-app container">
-      <h1 className="center blue-text">Todo List</h1>
-      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-      <AddTodo addTodo={this.addTodo}/>
+    return(
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+        <Route path="/home" component={Home}/ >
+        <Route path="/about" component={About}/ >
+        <Route path="/contact" component={Contact}/ >
+        <Route path="/:post_id" component={Post}/>
+        </Switch>
       </div>
+    </BrowserRouter>
     );
   }
 }
